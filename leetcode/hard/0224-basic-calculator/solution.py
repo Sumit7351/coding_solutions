4916d05e26c51da5512d97c79a6a77a96ea42496
Sumit7351 <sumitrana7351@gmail.com>
2026-08-22 16:31:14 +0530

    ✅ Basic Calculator | Hard | Python | Runtime: 111 ms (5.84%) | Memory: 23.8 MB | 2026-08-22

---
+++ leetcode/hard/0224-basic-calculator/solution.py
@@ -0,0 +1,73 @@
+class Solution:
+    def eval(self, a, b, op):
+        match op:
+            case "+":
+                return a + b
+            case "-":
+                return a - b
+
+    def convert_postfix(self, s):
+        stack = []
+        output = []
+        i = 0
+        prev = None
+
+        while i < len(s):
+
+            if s[i].isdigit():
+                num = ""
+
+                while i < len(s) and s[i].isdigit():
+                    num += s[i]
+                    i += 1
+
+                output.append(num)
+                prev = "num"
+                continue
+
+            elif s[i] == "(":
+                stack.append("(")
+                prev = "("
+
+            elif s[i] in "+-":
+
+                # Unary minus
+                if s[i] == "-" and (prev is None or prev == "(" or prev == "op"):
+                    output.append("0")
+
+                while stack and stack[-1] != "(":
+                    output.append(stack.pop())
+
+                stack.append(s[i])
+                prev = "op"
+
+            elif s[i] == ")":
+                while stack and stack[-1] != "(":
+                    output.append(stack.pop())
+
+                stack.pop()
+                prev = "num"
+
+            i += 1
+
+        while stack:
+            output.append(stack.pop())
+
+        return output
+
+    def calculate(self, s: str) -> int:
+        new = self.convert_postfix(s)
+
+        stack = []
+
+        for ch in new:
+            if ch.isdigit():
+                stack.append(int(ch))
+
+            elif ch in "+-":
+                val1 = stack.pop()
+                val2 = stack.pop()
+
+                stack.append(self.eval(val2, val1, ch))
+
+        return stack[-1]
